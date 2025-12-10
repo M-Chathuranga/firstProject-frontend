b3970f4ecc737c62fe617df6bb81b495964ca90a
M-Chathuranga <matheeshachaturanga@gmail.com>
2025-12-10 11:12:08 +0530

    Initialization 🚀

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import './App.css'
+import HomePage from './pages/homePage'
+import LoginPage from './pages/loginPage'
+import RegisterPage from './pages/registerPage'
+import AdminPage from './pages/adminPage'
+import TestPage from './pages/testPage'
+
+function App() {
+  
+
+  return (
+   <BrowserRouter>
+   <div className="w-full h-screen flex justify-center items-center">
+	   
+	   <Routes path="/">
+	        <Route path="" element={<HomePage/>} />
+		    <Route path="/login" element={<LoginPage/>}/>
+			<Route path="/register" element={<RegisterPage/>}/>
+		    <Route path="/admin/*" element={<AdminPage/>}/>
+			<Route path="/test" element={<TestPage/>} />
+	   </Routes>
+
+   </div>
+   </BrowserRouter>
+  )
+}
+
+export default App

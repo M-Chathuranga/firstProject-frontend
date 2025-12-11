9c41ba61067ec40022d1f10cb09ce3eee3f017ee
M-Chathuranga <matheeshachaturanga@gmail.com>
2025-12-11 22:11:13 +0530

    A login page was created and connected to the backend. ✏️

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,28 @@
-
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import './App.css'
-import HomePage from './pages/homePage'
-import LoginPage from './pages/loginPage'
-import RegisterPage from './pages/registerPage'
-import AdminPage from './pages/adminPage'
-import TestPage from './pages/testPage'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import HomePage from "./pages/homePage";
+import LoginPage from "./pages/loginPage";
+import RegisterPage from "./pages/registerPage";
+import AdminPage from "./pages/adminPage";
+import TestPage from "./pages/testPage";
+import { Toaster } from "react-hot-toast";
 
 function App() {
-  
-
-  return (
-   <BrowserRouter>
-   <div className="w-full h-screen flex justify-center items-center">
-	   
-	   <Routes path="/">
-	        <Route path="" element={<HomePage/>} />
-		    <Route path="/login" element={<LoginPage/>}/>
-			<Route path="/register" element={<RegisterPage/>}/>
-		    <Route path="/admin/*" element={<AdminPage/>}/>
-			<Route path="/test" element={<TestPage/>} />
-	   </Routes>
-
-   </div>
-   </BrowserRouter>
-  )
+	return (
+		<BrowserRouter>
+			<div className="w-full h-screen flex justify-center items-center">
+					<Toaster position="top-right"/>
+					<Routes path="/">
+						<Route path="/" element={<HomePage/>}/>
+						<Route path="/login" element={<LoginPage/>}/>
+						<Route path="/test" element={<TestPage/>}/>
+						<Route path="/register" element={<RegisterPage/>}/>
+						<Route path="/admin/*" element={<AdminPage/>}/>
+					</Routes>
+				
+			</div>
+		</BrowserRouter>
+	);
 }
 
-export default App
+export default App;

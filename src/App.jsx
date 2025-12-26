4753fbbd9b3aba635913c3661820193288ee6d26
M-Chathuranga <matheeshachaturanga@gmail.com>
2025-12-26 19:29:30 +0530

    Add Google login, reset password, OTP, and mobile responsive design ✏️

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,32 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import HomePage from "./pages/homePage";
 import LoginPage from "./pages/loginPage";
 import RegisterPage from "./pages/registerPage";
 import AdminPage from "./pages/adminPage";
 import TestPage from "./pages/testPage";
 import { Toaster } from "react-hot-toast";
 import ClientWebPage from "./pages/client/clientPage";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import ForgetPasswordPage from "./pages/client/forgetPassword";
+const clientId =(import.meta.env.VITE_GOOGLE_CID);
 
 function App() {
 	return (
 		<BrowserRouter>
-			<div className="w-full h-screen flex justify-center items-center">
-					<Toaster position="top-right"/>
+			<GoogleOAuthProvider clientId={clientId}>
+				<div className="w-full h-screen flex justify-center items-center bg-primary text-secondary">
+					<Toaster position="top-right" />
 					<Routes path="/">
-					    <Route path="/login" element={<LoginPage/>}/>
-						<Route path="/test" element={<TestPage/>}/>
-						<Route path="/register" element={<RegisterPage/>}/>
-						<Route path="/admin/*" element={<AdminPage/>}/>
-						<Route path="/*" element={<ClientWebPage/>}/>
+						<Route path="/login" element={<LoginPage />} />
+						<Route path="/test" element={<TestPage />} />
+						<Route path="/register" element={<RegisterPage />} />
+						<Route path="/admin/*" element={<AdminPage />} />
+						<Route path="/forget" element={<ForgetPasswordPage/>}/>
+						<Route path="/*" element={<ClientWebPage />} />
 					</Routes>
-				
-			</div>
+				</div>
+			</GoogleOAuthProvider>
 		</BrowserRouter>
 	);
 }
--- src/pages/client/productsPage.jsx
+++ src/pages/client/productsPage.jsx
@@ -3,42 +3,54 @@ import { useEffect, useState } from "react";
 import Loader from "../../components/loader";
 import ProductCard from "../../components/productCard";
 
-export default function ProductsPage(){
+export default function ProductsPage() {
+	const [products, setProducts] = useState([]);
+	const [loading, setLoading] = useState(true);
+	const [query, setQuery] = useState("");
 
-    const [products,setProducts] = useState([])
-    const [loading,setLoading] = useState(true)
+	useEffect(() => {
+		if (loading) {
+			if (query == "") {
+				axios
+					.get(import.meta.env.VITE_BACKEND_URL + "/api/products")
+					.then((res) => {
+						setProducts(res.data);
+						setLoading(false);
+					});
+			} else {
+				axios
+					.get(import.meta.env.VITE_BACKEND_URL + "/api/products/search/"+query)
+					.then((res) => {
+						setProducts(res.data);
+						setLoading(false);
+					});
+			}
+		}
+	}, [loading]);
 
-    useEffect(
-        ()=>{
-            if(loading){
-                axios.get(import.meta.env.VITE_BACKEND_URL+"/api/products").then(
-                    (res)=>{
-                        setProducts(res.data)
-                        setLoading(false)
-                    }
-                )
-            }
-        },
-        [loading]
-    )
-
-
-    return(
-        <div className="w-full h-full ">
-            {
-                loading? <Loader/> :
-                <div className="w-full  flex flex-wrap gap-10 justify-center items-center p-5">
-                    {
-                        products.map(
-                            (product)=>{
-                                return(
-                                    <ProductCard key={product.productId} product={product}/>
-                                )
-                            }
-                        )
-                    }
-                </div>
-            }
-        </div>
-    )
+	return (
+		<div className="w-full h-full ">
+            <div className="w-full h-[100px] flex justify-center items-center">
+                <input
+                    type="text"
+                    placeholder="Search products..."
+                    value={query}
+                    onChange={(e) => {
+                        setQuery(e.target.value);
+                        setLoading(true);
+                    }}
+                    className="w-[400px] h-[40px] border border-gray-300 rounded-lg p-2"
+                />
+            </div>
+			{loading ? (
+				<Loader />
+			) : (
+				<div className="w-full  flex flex-wrap gap-[40px] justify-center items-center p-[20px]">
+					{products.map((product) => {
+						return <ProductCard key={product.productId} product={product} />;
+					})}
+				</div>
+			)}
+		</div>
+	);
 }
